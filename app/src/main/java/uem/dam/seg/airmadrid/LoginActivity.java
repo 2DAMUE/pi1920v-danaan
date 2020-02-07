0e8cc9cf1ef788b0bc61475200f52f5d26fd3bbb
andrea <andreaaa5357@gmail.com>
2020-02-07 11:48:25 +0100

    -implementaciones -login -registro -añadido strings

--- app/src/main/java/uem/dam/seg/airmadrid/LoginActivity.java
+++ app/src/main/java/uem/dam/seg/airmadrid/LoginActivity.java
@@ -1,27 +1,89 @@
 package uem.dam.seg.airmadrid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    static final String CLAVE_EMAIL = "EMAIL";
+
+    private FirebaseAuth fa;
+    private FirebaseUser fu;
+
+    EditText etEmail;
+    EditText etPwd;
+
+    String email;
+    String pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fa = FirebaseAuth.getInstance();
+        fu = fa.getCurrentUser();
+
+        etEmail = findViewById(R.id.etEmail);
+        etPwd = findViewById(R.id.etContrasena);
+
+        if (fu != null) {
+            etEmail.setText(fu.getEmail());
+        }
     }
 
     public void entrar(View view) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        String msj = validarDatos();
+
+        if (msj != null) {
+            Toast.makeText(this, msj, Toast.LENGTH_LONG).show();
+        } else {
+            fa.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        fu = fa.getCurrentUser();
+
+                        // limpiamos para que al retroceder al segundo activity salgan los campos vacíos
+                        etEmail.setText("");
+                        etPwd.setText("");
+
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        i.putExtra(CLAVE_EMAIL, fu.getEmail());
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(LoginActivity.this, getString(R.string.msj_no_accede), Toast.LENGTH_SHORT).show();
+                    } } });
+        }
 
     }
 
     public void crearCuenta(View view) {
         Intent i = new Intent(this, RegistroActivity.class);
         startActivity(i);
+    }
+
+    private String validarDatos() {
+        email = etEmail.getText().toString().trim();
+        pwd = etPwd.getText().toString().trim();
+
+        String msj = null;
+        if (email.isEmpty() || pwd.isEmpty()) {
+            msj = getString(R.string.no_datos);
+        }
+
+        return msj;
     }
 }
--- app/src/main/java/uem/dam/seg/airmadrid/RegistroActivity.java
+++ app/src/main/java/uem/dam/seg/airmadrid/RegistroActivity.java
@@ -1,26 +1,87 @@
 package uem.dam.seg.airmadrid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private FirebaseAuth fa;
+    private FirebaseUser fu;
+
+    EditText etNomCom;
+    EditText etEmail;
+    EditText etPwd;
+
+    String nomCom;
+    String email;
+    String pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        fa = FirebaseAuth.getInstance();
+        fu = fa.getCurrentUser();
+
+        etNomCom = findViewById(R.id.etNombreCompReg);
+        etEmail = findViewById(R.id.etMailReg);
+        etPwd = findViewById(R.id.etPassReg);
+
+        if (fu != null) {
+            etEmail.setText(fu.getEmail());
+        }
     }
 
     public void crearCuentaReg(View view) {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
+        String msj = validarDatos();
+
+        if (msj != null) {
+            Toast.makeText(this, msj, Toast.LENGTH_LONG).show();
+        } else {
+            // registrar al usuario
+            fa.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        fu = fa.getCurrentUser();
+                        /*Intent i = new Intent(RegistroActivity.this, LoginActivity.class);
+                        startActivity(i);*/
+                        Toast.makeText(RegistroActivity.this, getString(R.string.msj_registrado), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegistroActivity.this, getString(R.string.msj_no_registrado), Toast.LENGTH_SHORT).show();
+                    } } });
+
+        }
     }
 
     public void iniciarSesion(View view) {
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
+    }
+
+    private String validarDatos() {
+        //nomCom = etNomCom.getText().toString().trim();
+        email = etEmail.getText().toString().trim();
+        pwd = etPwd.getText().toString().trim();
+
+        String msj = null;
+        if (/*nomCom.isEmpty() ||*/ email.isEmpty() || pwd.isEmpty()) {
+            msj = getString(R.string.no_datos);
+        }
+
+        return msj;
     }
 }
