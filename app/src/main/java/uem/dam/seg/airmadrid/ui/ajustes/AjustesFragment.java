c423cfe4fab154d8f83e5f71e7e8d1c2c2f891c7
andrea <andreaaa5357@gmail.com>
2020-03-11 18:38:36 +0100

    cambios en los ajustes

--- app/src/main/java/uem/dam/seg/airmadrid/ui/ajustes/AjustesFragment.java
+++ app/src/main/java/uem/dam/seg/airmadrid/ui/ajustes/AjustesFragment.java
@@ -1,17 +1,16 @@
 package uem.dam.seg.airmadrid.ui.ajustes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import uem.dam.seg.airmadrid.LoginActivity;
 import uem.dam.seg.airmadrid.R;
 
 public class AjustesFragment extends Fragment {
@@ -23,13 +22,6 @@ public class AjustesFragment extends Fragment {
         ajustesViewModel =
                 ViewModelProviders.of(this).get(AjustesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_ajustes, container, false);
-        final TextView textView = root.findViewById(R.id.text_ajustes);
-        ajustesViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
