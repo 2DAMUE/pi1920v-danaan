f37150c321864c0d751a1fcb05fb3f8f4614b70b
Daniel <danielcasildaalonso@gmail.com>
2020-03-11 18:47:11 +0100

    Comienzo Api

---
+++ app/src/main/java/uem/dam/seg/airmadrid/retrofitUtils/APICalidadDelAireService.java
@@ -0,0 +1,11 @@
+package uem.dam.seg.airmadrid.retrofitUtils;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APICalidadDelAireService {
+
+    public static final String BASE_URL = "https://www.w3schools.com/xml/";
+    //@GET("horario.xml")
+    //Call<Estaciones> obtenerDatos();
+}
